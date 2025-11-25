91cf2024f018ca33f97796eb594b6fc74ab87d18
PaOneX <opcmicrosoft@gmail.com>
2025-11-25 23:29:26 +0530

    connnect api

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("Hello, TVFlix!");
+
+
+function searchMovie(){
+
+
+    let txtMovie = document.getElementById("txtMovie").value;
+
+const requestOptions = {
+  method: "GET",
+  redirect: "follow"
+};
+
+fetch(`http://www.omdbapi.com/?t=${txtMovie}&apikey=afa85712`, requestOptions)
+  .then((response) => response.json())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
+}
