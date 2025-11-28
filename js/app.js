b992906a1362e32c3514e9f96c1a1471ac8a6c83
PaOneX <opcmicrosoft@gmail.com>
2025-11-29 00:27:21 +0530

    Fetch fi;m details to the card

--- js/app.js
+++ js/app.js
@@ -4,10 +4,15 @@ function searchMovie(){
   const option = document.getElementById("option").value;
   console.log(option);
   
+  if (!option) {
+    alert("select option");
+    return;
+  }
   
   // console.log("Hello, TVFlix!");
 
     const txtMovie = document.getElementById("txtMovie").value;
+    const txtyear = document.getElementById("txtyear").value;
 
 const requestOptions = {
   method: "GET",
@@ -17,7 +22,7 @@ const requestOptions = {
   switch (option) {
     case 'title':
       
-fetch(`http://www.omdbapi.com/?t=${txtMovie}&apikey=afa85712`, requestOptions)
+fetch(`http://www.omdbapi.com/?t=${txtMovie}&y=${txtyear}&apikey=afa85712`, requestOptions)
   .then((response) => response.json())
   .then((result) => {
     fetch("components/result/result.html").then(res=>res.text()).then(data=>{
@@ -25,6 +30,12 @@ fetch(`http://www.omdbapi.com/?t=${txtMovie}&apikey=afa85712`, requestOptions)
       document.getElementById("resultsRow").innerHTML = data;
       document.getElementById("txtTitle").innerText = result.Title;
       document.getElementById("txtPlot").innerText = result.Plot;
+      document.getElementById("txtActors").innerText = result.Actors;
+      document.getElementById("txtGenre").innerText = result.Genre;
+      document.getElementById("txtYear").innerText = result.Year;
+      document.getElementById("txtRating").innerText = result.Rated;
+      document.getElementById("txtDate").innerText = result.Released;
+      document.getElementById("txtRuntime").innerText = result.Runtime;
       document.getElementById("txtImg").innerHTML = ` <img src="${result.Poster}" class="card-img-top" alt="...">`;
     })
 
@@ -32,8 +43,29 @@ fetch(`http://www.omdbapi.com/?t=${txtMovie}&apikey=afa85712`, requestOptions)
   })
   .catch((error) => console.error(error));
       break;
-  default:
-    console.log("Hi");
+      
+      case 'imdb':
+        
+  fetch(`http://www.omdbapi.com/?i=${txtMovie}&apikey=afa85712`, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      fetch("components/result/result.html").then(res=>res.text()).then(data=>{
+        document.getElementById("resultdiv").classList.toggle("d-none");      
+        document.getElementById("resultsRow").innerHTML = data;
+        document.getElementById("txtTitle").innerText = result.Title;
+        document.getElementById("txtPlot").innerText = result.Plot;
+        document.getElementById("txtImg").innerHTML = ` <img src="${result.Poster}" class="card-img-top" alt="...">`;
+      })
+  
+  
+    })
+    .catch((error) => console.error(error));
+        break;
+      
+      
+
+
+      default:
       break;
   }
 
