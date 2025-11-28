ee6ec6dc6a76cd228a62cb4c034de7d0cae018ec
PaOneX <opcmicrosoft@gmail.com>
2025-11-28 20:28:27 +0530

    adding card to show the search result

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-
+let option = document.getElementById("option").value;
+// cons
 
 function searchMovie(){
   
@@ -13,6 +14,16 @@ const requestOptions = {
 
 fetch(`http://www.omdbapi.com/?t=${txtMovie}&apikey=afa85712`, requestOptions)
   .then((response) => response.json())
-  .then((result) => console.log(result))
+  .then((result) => {
+    fetch("components/result/result.html").then(res=>res.text()).then(data=>{
+      document.getElementById("resultdiv").classList.toggle("d-none");      
+      document.getElementById("resultsRow").innerHTML = data;
+      document.getElementById("txtTitle").innerText = result.Title;
+      document.getElementById("txtPlot").innerText = result.Plot;
+      document.getElementById("txtImg").innerHTML = ` <img src="${result.Poster}" class="card-img-top" alt="...">`;
+    })
+
+
+  })
   .catch((error) => console.error(error));
 }
